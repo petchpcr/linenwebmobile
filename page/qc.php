08e185f68b4289f6e876af30a214670393c25cbe
petchpcr <petchpcr@gmail.com>
2019-08-05 10:49:50 +0700

    logout android

--- page/qc.php
+++ page/qc.php
@@ -100,22 +100,31 @@
         }
 
         function logout(num){
-            swal({
-            title: '<?php echo $genarray['logout'][$language]; ?>',
-            text: '<?php echo $genarray['wantlogout'][$language]; ?>',
-            type: 'question',
-            showCancelButton: true,
-            showConfirmButton: true,
-            cancelButtonText: '<?php echo $genarray['isno'][$language]; ?>',
-            confirmButtonText: '<?php echo $genarray['yes'][$language]; ?>',
-            reverseButton:true,
-            }).then(function () {
+            if (num == 0) {
                 var data = {
-                    'Confirm': num,
+                    'Confirm': 1,
                     'STATUS': 'logout'
                 };
                 senddata(JSON.stringify(data));
-            });
+            }
+            else if (num == 1) {
+                swal({
+                title: '<?php echo $genarray['logout'][$language]; ?>',
+                text: '<?php echo $genarray['wantlogout'][$language]; ?>',
+                type: 'question',
+                showCancelButton: true,
+                showConfirmButton: true,
+                cancelButtonText: '<?php echo $genarray['isno'][$language]; ?>',
+                confirmButtonText: '<?php echo $genarray['yes'][$language]; ?>',
+                reverseButton:true,
+                }).then(function () {
+                    var data = {
+                        'Confirm': num,
+                        'STATUS': 'logout'
+                    };
+                    senddata(JSON.stringify(data));
+                });
+            }
         }
 
         function senddata(data) {
